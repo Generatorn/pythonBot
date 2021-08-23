cab02261f33ba3a6866ab19db30d993b77a69d64
Vit Constructor <ivanjarov1945@gmail.com>
2021-08-23 08:45:39 +0700

    Кнопки появились, но они экспортированы не из отдельного файла

--- hendlers.py
+++ hendlers.py
@@ -1,4 +1,4 @@
-from main import bot, dp
+from main import bot, dp, types
 import keyboards as kb
 from aiogram.types import Message
 from config import admin_id
@@ -20,3 +20,13 @@ async def process_start_command(message: Message):
 #    await message.answer("Привет!", reply_markup=kb.greet_kb)
     message.reply("Привет!", reply_markup=kb.greet_kb)
     print("hello")
+
+
+@dp.message_handler(commands="start")
+async def cmd_start(message: Message):
+    keyboard = types.ReplyKeyboardMarkup()
+    button_1 = types.KeyboardButton(text="С пюрешкой")
+    keyboard.add(button_1)
+    button_2 = "Без пюрешки"
+    keyboard.add(button_2)
+    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
