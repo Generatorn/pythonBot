1e3de82e2452083ec9189b3b76ec322fddf8683c
Vit Constructor <ivanjarov1945@gmail.com>
2021-08-26 06:11:11 +0700

    Наконец простая кнопка заработала

--- hendlers.py
+++ hendlers.py
@@ -15,18 +15,18 @@ async def send_to_admin(dp):
 
 
 # Обработчик кнопки
-@dp.message_handler(commands=['help'])
+@dp.message_handler(commands=['start'])
 async def process_start_command(message: Message):
-#    await message.answer("Привет!", reply_markup=kb.greet_kb)
-    message.reply("Привет!", reply_markup=kb.greet_kb)
-    print("hello")
+    await message.answer("Привет!", reply_markup=kb.greet_kb)
+    #message.reply("Привет!", reply_markup=kb.greet_kb)
+#    print("hello")
 
 
-@dp.message_handler(commands="start")
-async def cmd_start(message: Message):
-    keyboard = types.ReplyKeyboardMarkup()
-    button_1 = types.KeyboardButton(text="С пюрешкой")
-    keyboard.add(button_1)
-    button_2 = "Без пюрешки"
-    keyboard.add(button_2)
-    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+#@dp.message_handler(commands="start")
+#async def cmd_start(message: Message):
+#    keyboard = types.ReplyKeyboardMarkup()
+#    button_1 = types.KeyboardButton(text="С пюрешкой")
+#    keyboard.add(button_1)
+#    button_2 = "Без пюрешки"
+#    keyboard.add(button_2)
+#    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
